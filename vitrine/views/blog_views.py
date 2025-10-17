94063bb20c90be557bdbe540efbeda621d85496a
LordArimhan <bragawebdevelopment@gmail.com>
2025-10-17 16:02:49 -0300

    Criação de Views  para exibir todos os blogs

--- vitrine/views/blog_views.py
+++ vitrine/views/blog_views.py
@@ -5,6 +5,18 @@ from django.shortcuts import get_object_or_404
 
 
 
+
+
+
+def blog_list(request):
+    blogs = Blog.objects.filter(is_published=True).order_by('-created_at')
+
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'vitrine/blog_list.html', context)
+
+
 def blog_view(request, slug, blog_id):
     blog = get_object_or_404(Blog, slug=slug, id=blog_id)
 
@@ -12,3 +24,6 @@ def blog_view(request, slug, blog_id):
         'blog': blog
     }
     return render(request, 'vitrine/blog.html', context)
+
+
+
